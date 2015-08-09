1571648a50bdae6b4b626c997e725e60ad1caeaf
Bryan Bennett <bbenne10@gmail.com>
2015-08-09 19:22:25 -0400

    Retab a rc and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$1" != --force ]; then
-    echo "Please read the setup script and confirm that it doesn't break your system."
-    exit 1
+  echo "Please read the setup script and confirm that it doesn't break your system."
+  exit 1
 fi
 
 [ -z "$ROOT" ] && ROOT=
@@ -9,9 +9,9 @@ fi
 echo "==> Installing /sbin/rc, /etc/rc.conf, /etc/inittab"
 install -Dm755 rc "$ROOT"/sbin/rc
 if [ -f "$ROOT"/etc/rc.conf ]; then
-    echo ":: Warning: '$ROOT/etc/rc.conf' already exists!"
-    echo "   Moving it to '$ROOT/etc/rc.conf.backup'."
-    mv "$ROOT"/etc/rc.conf "$ROOT"/etc/rc.conf.backup
+  echo ":: Warning: '$ROOT/etc/rc.conf' already exists!"
+  echo "   Moving it to '$ROOT/etc/rc.conf.backup'."
+  mv "$ROOT"/etc/rc.conf "$ROOT"/etc/rc.conf.backup
 fi
 install -Dm644 rc.conf "$ROOT"/etc/rc.conf
 install -Dm644 inittab "$ROOT"/etc/inittab
@@ -23,7 +23,7 @@ install -Dm755 shutdown.sh "$ROOT/sbin/shutdown"
 
 echo "==> Linking busybox to /sbin/{init,halt,poweroff,reboot}"
 for i in init halt poweroff reboot; do
-    ln -sf $(which busybox) "$ROOT"/sbin/$i
+  ln -sf $(which busybox) "$ROOT"/sbin/$i
 done
 
 echo ":: Append 'init=/sbin/init' to your kernel line in your bootloader"
